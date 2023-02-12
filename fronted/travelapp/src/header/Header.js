cf9397b0d377b6ced373f0c63207323b12bc2728
deiu25 <d3iu25@gmail.com>
2023-02-12 21:03:09 +0200

    Dynamically Updating Header Links After Authentication

--- fronted/travelapp/src/header/Header.js
+++ fronted/travelapp/src/header/Header.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import { AppBar, Tab, Tabs, Toolbar } from "@mui/material";
 import ModeOfTravelIcon from "@mui/icons-material/ModeOfTravel";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const linksArr = ["home", "diaries", "auth"];
+const isLoggedInLinks = ["home", "diaries", "add", "profile"];
 
 const Header = () => {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
   const [value, setValue] = useState();
+
   return (
     <AppBar sx={{ bgcolor: "transparent", position: "sticky" }}>
       <Toolbar>
@@ -16,21 +20,37 @@ const Header = () => {
           onChange={(e, val) => setValue(val)}
           sx={{ ml: "auto", textDecoration: "none" }}
         >
-          {linksArr.map((link) => (
-            <Tab
-              LinkComponent={Link}
-              to={`/${link === "home" ? "" : link}`}
-              sx={{
-                textDecoration: "none",
-                ":hover": {
-                  textDecoration: "underline",
-                  textUnderlineOffset: "18px",
-                },
-              }}
-              key={link}
-              label={link}
-            />
-          ))}
+          {isLoggedIn
+            ? isLoggedInLinks.map((link) => (
+                <Tab
+                  LinkComponent={Link}
+                  to={`/${link === "home" ? "" : link}`}
+                  sx={{
+                    textDecoration: "none",
+                    ":hover": {
+                      textDecoration: "underline",
+                      textUnderlineOffset: "18px",
+                    },
+                  }}
+                  key={link}
+                  label={link}
+                />
+              ))
+            : linksArr.map((link) => (
+                <Tab
+                  LinkComponent={Link}
+                  to={`/${link === "home" ? "" : link}`}
+                  sx={{
+                    textDecoration: "none",
+                    ":hover": {
+                      textDecoration: "underline",
+                      textUnderlineOffset: "18px",
+                    },
+                  }}
+                  key={link}
+                  label={link}
+                />
+              ))}
         </Tabs>
       </Toolbar>
     </AppBar>
