73c5813e4312993c6633fb3dbfdd7674a138eaae
deiu25 <d3iu25@gmail.com>
2023-02-16 22:35:10 +0200

    Adding Delete Blogpost Functionality With Notifications

--- fronted/travelapp/src/diaries/DiaryItem.js
+++ fronted/travelapp/src/diaries/DiaryItem.js
@@ -1,25 +1,36 @@
 import {
+  Alert,
   Avatar,
   Card,
   CardActions,
   CardContent,
   CardHeader,
   IconButton,
+  Snackbar,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import EditLocationAltIcon from "@mui/icons-material/EditLocationAlt";
 import { Box } from "@mui/system";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import { Link } from "react-router-dom";
+import { postDelete } from "../api-helpers/helpers";
 
 const DiaryItem = ({ title, description, image, location, date, id, user }) => {
+  const [open, setOpen] = useState(false);
   const isLoggedInUser = () => {
     if (localStorage.getItem("userId") === user) {
       return true;
     }
     return false;
+  };
+
+  const handleDelete = () => {
+    postDelete(id)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+    setOpen(true);
   };
 
   return (
@@ -70,11 +81,24 @@ const DiaryItem = ({ title, description, image, location, date, id, user }) => {
           <IconButton LinkComponent={Link} to={`/post/${id}`} color="warning">
             <EditIcon />
           </IconButton>
-          <IconButton color="error">
+          <IconButton onClick={handleDelete} color="error">
             <DeleteForeverIcon />
           </IconButton>
         </CardActions>
       )}
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={() => setOpen(false)}
+      >
+        <Alert
+          onClose={() => setOpen(false)}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          This is a success message!
+        </Alert>
+      </Snackbar>
     </Card>
   );
 };
