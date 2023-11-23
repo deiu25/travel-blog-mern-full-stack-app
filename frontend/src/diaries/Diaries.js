fe03bdeba8cac63ad790a87e1c95183fb62ce7df
Badulescu Andrei <d3iu25@gmail.com>
2023-11-23 21:52:20 +0200

    delete pagination

--- frontend/src/diaries/Diaries.js
+++ frontend/src/diaries/Diaries.js
@@ -1,30 +1,15 @@
-import { Box, CircularProgress, Pagination } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { getAllPosts } from "../api-helpers/helpers";
 import DiaryItem from "./DiaryItem";
 
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-
 const Diaries = () => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const postsPerPage = isSmallScreen ? 10 : 10;
+  const [posts, setPosts] = useState();
 
   useEffect(() => {
     getAllPosts()
-      .then((data) => {
-        setPosts(data?.posts);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
+      .then((data) => setPosts(data?.posts))
+      .catch((err) => console.log(err));
   }, []);
 
   const handlePostDelete = (deletedPostId) => {
@@ -42,38 +27,20 @@ const Diaries = () => {
       paddingBottom={10}
       sx={{ backgroundColor: "white" }}
     >
-      {isLoading ? (
-        <CircularProgress />
-      ) : (
-        <>
-          {posts
-            .slice((page - 1) * postsPerPage, page * postsPerPage)
-            .map((item, index) => (
-              <DiaryItem
-                date={new Date(`${item.date}`).toLocaleDateString()}
-                description={item.description}
-                images={item.images}
-                id={item._id}
-                location={item.location}
-                title={item.title}
-                key={index}
-                user={item.user}
-                onPostDelete={handlePostDelete}
-              />
-            ))}
-        </>
-      )}
-
-      {!isLoading && (
-        <Box display="flex" justifyContent="center" width="100%" mt={4} mb={2}>
-          <Pagination
-            count={Math.ceil(posts.length / postsPerPage)}
-            page={page}
-            onChange={(event, value) => setPage(value)}
-            sx={{ backgroundColor: 'white' }}
+      {posts &&
+        posts.map((item, index) => (
+          <DiaryItem
+            date={new Date(`${item.date}`).toLocaleDateString()}
+            description={item.description}
+            images={item.images}
+            id={item._id}
+            location={item.location}
+            title={item.title}
+            key={index}
+            user={item.user}
+            onPostDelete={handlePostDelete}
           />
-        </Box>
-      )}
+        ))}
     </Box>
   );
 };
